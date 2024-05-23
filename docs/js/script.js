f91b0311a4c2958e94c26522d5f1e0e09bd85160
alina onmaster <alina.onmaster@gmail.com>
2024-05-23 13:41:39 +0300

    JS Проверка на поддержку браузером webp

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,17 @@
+/* Проверка на поддержку браузером webp */
+function testWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+    let className = support === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+});
+
 /* Ширина вьюпорта */
 let windowWidth = document.documentElement.clientWidth;
 
